cf492fff8cd400b2a665054da102d31cf02add7e
RebekaRibeiroGomes <95870224+RebekaRibeiroGomes@users.noreply.github.com>
2024-09-12 19:41:36 -0300

    Update livro.h

--- livro.h
+++ livro.h
@@ -4,10 +4,10 @@
 #include "estrutura.h"
 
 Livro* criaLivro(char* titulo, char* autor, char* ISBN, char* anoPublicacao);
-void adicionarLivro(FILE* saida, Lista* l, char* titulo, char* autor, char* ISBN, char* anoPublicacao);
+void adicionarLivro(FILE* output, Lista* l, char* titulo, char* autor, char* ISBN, char* anoPublicacao);
 Livro* buscarLivro(FILE* output, Lista* l, char* busca);
-int checkout(Lista* l, char* ISBN);
-int checkin(Lista* l, char* ISBN);
+int checkout(FILE* output, Lista* l, char* ISBN);
+int checkin(FILE* output, Lista* l, char* ISBN);
 void exibir1Livro(FILE* output, Livro* l);
 
 #endif
