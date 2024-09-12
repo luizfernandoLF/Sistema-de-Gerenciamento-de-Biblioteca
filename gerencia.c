78a9e8dfb2f76a5864fe9901b2eaad024c13df22
RebekaRibeiroGomes <95870224+RebekaRibeiroGomes@users.noreply.github.com>
2024-09-12 00:37:46 -0300

    Update gerencia.c

--- gerencia.c
+++ gerencia.c
@@ -4,4 +4,32 @@
 #include <stdbool.h>
 #include "livro.h"
 
-int main (){}
+int main(int argc, char* argv[]) {
+    // Abrindo os arquivos de entrada e saída
+    FILE* infile = fopen(arg[1], "r");
+    FILE* outfile = fopen(arg[2], "w");
+
+    if (infile == NULL || outfile == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    char CMD[30];
+    char titulo[100];
+    char autor[100];
+    char ISBN[50];
+    char ano[6];
+    char separador[4];
+
+    // Processando cada linha do arquivo de entrada
+    while (fscanf(infile, "%s %[^;]; %[^;]; %[^;]; %s", CMD, ISBN, titulo, autor, ano) != EOF) {
+        // Escrevendo no arquivo de saída
+        fprintf(outfile, "%s|%s|%s|%s|%s\n", CMD, ISBN, titulo, autor, ano);
+    }
+
+    // Fechando os arquivos
+    fclose(infile);
+    fclose(outfile);
+
+    return 0;
+}
