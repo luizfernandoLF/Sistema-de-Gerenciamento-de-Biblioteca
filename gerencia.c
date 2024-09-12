2a1d02d5380ae647995938ab85412779adccae49
RebekaRibeiroGomes <95870224+RebekaRibeiroGomes@users.noreply.github.com>
2024-09-12 19:41:02 -0300

    Update gerencia.c

--- gerencia.c
+++ gerencia.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "livro.h"
 
@@ -26,6 +27,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Variáveis para armazenar os dados lidos
+    char divisor[100] = "\n*****************************************************\n";
     Lista* lista = criaLista();
     char busca[100];
     char CMD[30];
@@ -33,36 +35,47 @@ int main(int argc, char *argv[]) {
     char autor[100];
     char ISBN[50];
     char ano[6];
-    char separador[4];
 
     // Lendo e processando o arquivo
     while (fscanf(infile, "%s", CMD) != EOF) {
+        fprintf(outfile, divisor);
         if(strcmp(CMD, "ADD") == 0) {
             if (fscanf(infile, " %[^;];", ISBN) == 1 &&
                 fscanf(infile, " %[^;];", titulo) == 1 &&
                 fscanf(infile, " %[^;];", autor) == 1 &&
                 fscanf(infile, " %s", ano) == 1) {
+                //fprintf(outfile, divisor);
                 adicionarLivro(outfile, lista, titulo, autor, ISBN, ano);
             } else {
+                //fprintf(outfile, divisor);
                 fprintf(outfile, "Erro ao ler as informações do livro!\n\n");
             }
-            //adicionarLivro(outfile, lista, titulo, autor, ISBN, ano);
         } else if (strcmp(CMD, "SEARCH") == 0) {
+            //fprintf(outfile, divisor);
+            fprintf(outfile, "Buscando livro...\n");
             if (fscanf(infile, " %[^;];", busca) == 1) {
-                buscarLivro(outfile, lista, busca);
+                if (buscarLivro(outfile, lista, busca) != NULL) {
+                    fprintf(outfile, "Livro encontrado!\n");
+                    exibir1Livro(outfile, buscarLivro(outfile, lista, busca));
+                }
             } else {
                 fprintf(outfile, "Erro ao ler as informações de busca do livro!\n\n");
             }
+        } else if (strcmp(CMD, "CHECK_OUT") == 0) {
+            if (fscanf(infile, " %[^;];", busca) == 1) {
+                //fprintf(outfile, divisor);
+                checkout(outfile, lista, busca);
+            } else {
+                //fprintf(outfile, divisor);
+                fprintf(outfile, "Erro ao fazer a retirada do livro!\n\n");
+            }
+        } else if (strcmp(CMD, "CHECK_IN") == 0) {
+            if (fscanf(infile, " %[^;];", busca) == 1) {
+                checkin(outfile, lista, busca);
+            } else {
+                fprintf(outfile, "Erro ao fazer a devolução do livro!\n\n");
+            }
         }
-        // } else if (strcmp(CMD, "CHECK_OUT") == 0) {
-
-        // } else if (strcmp(CMD, "CHECK_IN") == 0) {
-
-        // } else {
-        //     printf("Comando não encontrado!");
-        // }
-        // Escrevendo no arquivo de saída
-        //fprintf(outfile, "%s|%s|%s|%s|%s\n", CMD, ISBN, titulo, autor, ano);
     }
 
     // Fechando os arquivos
