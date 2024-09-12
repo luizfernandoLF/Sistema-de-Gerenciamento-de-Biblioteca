4a6e474454b05f2d593afcceba6e3179523503e9
RebekaRibeiroGomes <95870224+RebekaRibeiroGomes@users.noreply.github.com>
2024-09-12 14:15:01 -0300

    Update livro.h

--- livro.h
+++ livro.h
@@ -1,22 +1,14 @@
+
 #include <stdbool.h>
-
-// typedef struct Livro {
-//     char titulo[100];
-//     char autor[40];
-//     char ISBN[50];
-//     char anoPublicacao[6];
-//     bool status;
-//     struct Livro* prox;
-// } livro;
-
-// typedef struct lista {
-//     livro* prim;
-//     livro* ult;
-//     int tamanho;
-// } lista;
+#include "estrutura.h"
 
 
-Livro* crialivro(char* titulo, char* autor, char* ISBN, char* anoPublicacao);
-void alterarLivro(Livro* lv);
-void exibir1Livro(Livro* lv);
+Livro* criaLivro(char* titulo, char* autor, char* ISBN, char* anoPublicacao);
+void adicionarLivro(Lista* l, char* titulo, char* autor, char* ISBN, char* anoPublicacao);
+bool isISBN(char* input);
+Livro* buscarLivro(Lista* l, char* busca);
 bool validarISBN(char* ISBN);
+int checkout(Lista* l, char* ISBN);
+int checkin(Lista* l, char* ISBN);
+// void exibirLivros(Lista* l);
+void exibir1Livro(Livro* l);
