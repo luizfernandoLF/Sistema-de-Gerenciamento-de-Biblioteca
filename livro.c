fc1ed39fe8b688e6b97e55e102265977f9ba8ff1
RebekaRibeiroGomes <95870224+RebekaRibeiroGomes@users.noreply.github.com>
2024-09-12 19:41:26 -0300

    Update livro.c

--- livro.c
+++ livro.c
@@ -26,49 +26,61 @@ void adicionarLivro(FILE* output, Lista* l, char* titulo, char* autor, char* ISB
     }
     l->tamanho++;
 
-    fprintf(output, "Livro adicionado com exito! Dados do livro:\n"
-            "Titulo: %s\n"
+    fprintf(output, "Livro adicionado com êxito!\nDados do livro:\n"
+            "Título: %s\n"
             "Autor: %s\n"
-            "ISBN:%s\n"
-            "Ano de publicacao: %s\n"
-            "Status de disponibilidade: %s\n\n\n",
+            "ISBN: %s\n"
+            "Ano de publicação: %s\n"
+            "Status de disponibilidade: %s\n\n",
             novo->titulo, novo->autor, novo->ISBN, novo->anoPublicacao,
-            novo->status ? "Disponivel" : "Indisponivel");
+            novo->status ? "Disponível" : "Indisponível");
 }
 
 Livro* buscarLivro(FILE* output, Lista* l, char* busca) {
     Livro* at = l->prim;
     while (at != NULL) {
         if (strcmp(at->ISBN, busca) == 0 || strcmp(at->titulo, busca) == 0 || strcmp(at->autor, busca) == 0) {
-            exibir1Livro(output, at);
+            //fprintf(output, "Busca com sucesso!\n");
+            //exibir1Livro(output, at);
             return at;
         }
         at = at->prox;
     }
+
+    fprintf(output, "Livro não encontrado!\n\n");
     return NULL;
 }
 
-int checkout(Lista* l, char* ISBN) {
-    Livro* lv = buscarLivro(l, ISBN);
+int checkout(FILE* output, Lista* l, char* ISBN) {
+    Livro* lv = buscarLivro(output, l, ISBN);
     if (lv != NULL && lv->status == true) {
         lv->status = false;
+        fprintf(output, "Livro retirado para empréstimo com sucesso!\n");
+        exibir1Livro(output, lv);
         return 1;
+    } else if (lv->status == false) {
+        fprintf(output, "Livro indisponível para empréstimo!\n");
+        return 0;
     }
+    fprintf(output, "Erro ao fazer a retirada do livro!\n\n");
     return 0;
 }
 
-int checkin(Lista* l, char* ISBN) {
-    Livro* lv = buscarLivro(l, ISBN);
+int checkin(FILE* output, Lista* l, char* ISBN) {
+    Livro* lv = buscarLivro(output, l, ISBN);
     if (lv != NULL && lv->status == false) {
         lv->status = true;
+        fprintf(output, "Livro devolvido com sucesso!\n");
+        exibir1Livro(output, lv);
         return 1;
     }
+    fprintf(output, "Erro ao fazer a devolução do livro!\n\n");
     return 0;
 }
 
 void exibir1Livro(FILE* output, Livro* l) {
     if (l != NULL) {
-        printf(
+        fprintf(output,
             "Dados do livro:\n"
             "Titulo: %s\n"
             "Autor: %s\n"
@@ -78,6 +90,6 @@ void exibir1Livro(FILE* output, Livro* l) {
             l->titulo, l->autor, l->ISBN, l->anoPublicacao,
             l->status ? "Disponivel" : "Indisponivel");
     } else {
-        printf("Livro não encontrado.\n\n\n");
+        fprintf(output, "Livro não encontrado.\n\n\n");
     }
 }
